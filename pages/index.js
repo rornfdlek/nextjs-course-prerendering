b1921a9f21119af919a2b57aadec77545d544848
hnee <rornfdlek@naver.com>
2022-03-06 15:47:18 +0900

    94. ISR: A Look Behind The Scenes

--- pages/index.js
+++ pages/index.js
@@ -14,13 +14,16 @@ function HomePage(props) {
 export async function getStaticProps() {
   // cwd = current working directory
   // 여기선  cwd = project root folder 
-  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json') ; // dummy-backend.json 파일의 절대경로를 반환
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json'); // dummy-backend.json 파일의 절대경로를 반환
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
 
-  return { props: {
-    products: data.products
-  } };
+  return {
+    props: {
+      products: data.products
+    },
+    revalidate: 10
+  };
 }
 
 export default HomePage;
