781120520b351290411cf11576c9f600825ae8d1
hnee <rornfdlek@naver.com>
2022-03-07 15:16:01 +0900

    107. getServerSideProps: Behind The Scenes

--- pages/products/[pid].js
+++ pages/products/[pid].js
@@ -18,16 +18,26 @@ function ProductDetailPage(props) {
     );
 }
 
+async function getData() {
+    const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json'); // dummy-backend.json 파일의 절대경로를 반환
+    const jsonData = await fs.readFile(filePath);
+    const data = JSON.parse(jsonData);
+
+    return data;
+}
+
 export async function getStaticProps(context) {
     const { params } = context;
 
     const productId = params.pid;
 
-    const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json'); // dummy-backend.json 파일의 절대경로를 반환
-    const jsonData = await fs.readFile(filePath);
-    const data = JSON.parse(jsonData);
+    const data = await getData();
 
     const product = data.products.find(product => product.id === productId);
+
+    if(!product) {
+        return { notFound: true };
+    }
 
     return {
         props: {
@@ -37,13 +47,14 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
+    const data = await getData();
+
+    const ids = data.products.map(product => product.id);
+    const pathsWithParams = ids.map(id => ({params: {pid: id}}));
+
     return {
-        paths: [
-            { params: {pid: 'p1'}},
-            { params: {pid: 'p2'}},
-            { params: {pid: 'p3'}},
-        ],
-        fallback: false
+        paths: pathsWithParams,
+        fallback: true
     };
 }
 
